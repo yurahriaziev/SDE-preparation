fe78725a9eddc232e226fff0974db18b7a5c5919
yurahriaziev <yura.hriaziev@gmail.com>
2024-11-25 10:28:10 -0500

    Binary tree visualization, 4 ways of traversing: Inorder, Preorder, Reversed Inorder, Postorder

--- Practice/binary_tree.py
+++ Practice/binary_tree.py
@@ -7,6 +7,24 @@ class BinaryTree:
             self.inorder_trav(node.left)
             print(node.val, end=" ")
             self.inorder_trav(node.right)
+    
+    def preorder_trav(self, node):
+        if node:
+            print(node.val, end=" ")
+            self.preorder_trav(node.left)
+            self.preorder_trav(node.right)
+
+    def rev_inorder_trav(self, node):
+        if node:
+            self.rev_inorder_trav(node.right)
+            print(node.val, end=' ')
+            self.rev_inorder_trav(node.left)
+
+    def postorder_trav(self, node):
+        if node:
+            self.postorder_trav(node.left)
+            self.postorder_trav(node.right)
+            print(node.val, end=' ')
 
     # adding a new node to the tree
     def insert(self, current, val):
@@ -67,17 +85,21 @@ class VisualizeTree:
             if node.right:
                 pygame.draw.line(self.screen, self.edge_color, (x, y), (x+x_offset, y+y_offset), 2)
                 self.draw_tree(node.right, x+x_offset, y+y_offset, x_offset//2, y_offset, visited)
-
-            color = self.visited_color if node in visited else self.default_color
+            if visited:
+                color = self.visited_color if node in visited else self.default_color
+            else:
+                color = self.default_color
             pygame.draw.circle(self.screen, color, (x, y), self.node_rad)
-            node_val_text = self.font.render(str(node.val), True, (0,0,0))
-            self.screen.blit(node_val_text, (x - self.node_rad//2, y-self.node_rad//2))
+            node_val_text = self.font.render(str(node.val), True, (255,255,255))
+            self.screen.blit(node_val_text, (x - self.node_rad//2+3, y-self.node_rad//2+2))
         pygame.display.update()
 
     def draw_instructions(self):
         instructions = [
             "Press 1 to show 'Inorder Traverse'",
-            "Press 2 to pause the traverse",
+            "Press 2 to show 'Preorder Traverse'",
+            "Press 3 to show 'Reversed Inorder Traverse'",
+            "Press 4 to show 'Postorder Traverse'",
             "Press 'q' to quit",
         ]
         y_pos = self.height - len(instructions) * 30
@@ -91,10 +113,6 @@ class VisualizeTree:
         if node:
             self.inorder_trav(node.left, visited)
 
-            visited.add(node)
-            self.screen.fill((255, 255, 255))
-            self.draw_tree(self.tree.root, self.width//2, 50, 200, 70, visited)
-            self.draw_instructions()
 
             while self.is_paused:
                 for event in pygame.event.get():
@@ -107,15 +125,111 @@ class VisualizeTree:
                     elif keys[pygame.K_q]:
                         pygame.quit()
                         exit()
+
+            visited.add(node)
+            self.screen.fill((255, 255, 255))
+            self.draw_tree(self.tree.root, self.width//2, 50, 200, 70, visited)
+            self.draw_instructions()
             
             time.sleep(0.5)
 
             self.inorder_trav(node.right, visited)
         
+    def preorder_trav(self, node, visited):
+        if node:
+            while self.is_paused:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        exit()
+                    keys = pygame.key.get_pressed()
+                    if keys[pygame.K_1]:
+                        self.is_paused = False
+                    elif keys[pygame.K_q]:
+                        pygame.quit()
+                        exit()
+
+            visited.add(node)
+            self.screen.fill((255, 255, 255))
+            self.draw_tree(self.tree.root, self.width//2, 50, 200, 70, visited)
+            self.draw_instructions()
+            time.sleep(0.8)
+
+            self.preorder_trav(node.left, visited)
+            self.preorder_trav(node.right, visited)
+    
+    def rev_inorder_trav(self, node, visited):
+        if node:
+            self.rev_inorder_trav(node.right, visited)
+            while self.is_paused:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        exit()
+                    keys = pygame.key.get_pressed()
+                    if keys[pygame.K_1]:
+                        self.is_paused = False
+                    elif keys[pygame.K_q]:
+                        pygame.quit()
+                        exit()
+
+            visited.add(node)
+            self.screen.fill((255, 255, 255))
+            self.draw_tree(self.tree.root, self.width//2, 50, 200, 70, visited)
+            self.draw_instructions()
+            time.sleep(0.8)
+            self.rev_inorder_trav(node.left, visited)
+
+    def postorder_trav(self, node, visited):
+        if node:
+            self.postorder_trav(node.left, visited)
+            self.postorder_trav(node.right, visited)
+
+            while self.is_paused:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        exit()
+                    keys = pygame.key.get_pressed()
+                    if keys[pygame.K_1]:
+                        self.is_paused = False
+                    elif keys[pygame.K_q]:
+                        pygame.quit()
+                        exit()
+
+            visited.add(node)
+            self.screen.fill((255, 255, 255))
+            self.draw_tree(self.tree.root, self.width//2, 50, 200, 70, visited)
+            self.draw_instructions()
+            time.sleep(0.8)
+
+
+
+    def visualize_binary_tree(self):
+        self.setup_pygame()
+        self.screen.fill((255, 255, 255))
+        self.draw_tree(self.tree.root, self.width // 2, 50, 200, 70, None)
+        self.draw_instructions()
+
     def visualize_inorder_trav(self):
         visited = set()
         self.setup_pygame()
         self.inorder_trav(self.tree.root, visited)
+
+    def visualize_preorder_trav(self):
+        visited = set()
+        self.setup_pygame()
+        self.preorder_trav(self.tree.root, visited)
+
+    def visualize_revinorder_trav(self):
+        visited = set()
+        self.setup_pygame()
+        self.rev_inorder_trav(self.tree.root, visited)
+
+    def visualize_postorder_trav(self):
+        visited = set()
+        self.setup_pygame()
+        self.postorder_trav(self.tree.root, visited)
 
 tree = BinaryTree(10)
 tree.insert(tree.root, 5)
@@ -123,20 +237,21 @@ tree.insert(tree.root, 15)
 tree.insert(tree.root, 3)
 tree.insert(tree.root, 7)
 tree.insert(tree.root, 12)
+tree.insert(tree.root, 13)
+tree.insert(tree.root, 11)
+tree.insert(tree.root, 9)
 tree.insert(tree.root, 18)
 tree.insert(tree.root, 2)
 
-def start():
+def start_visual():
     pygame.init()
     visualizer = VisualizeTree(tree)
 
     run = True
     traversing = True
 
+    visualizer.visualize_binary_tree()
     while run:
-        if traversing:
-            visualizer.visualize_inorder_trav()
-            traversing = False
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -144,14 +259,40 @@ def start():
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_1]:
-            traversing = True
+            visualizer.visualize_inorder_trav()
+            time.sleep(0.5)
+            visualizer.visualize_binary_tree()
 
         if keys[pygame.K_2]:
-            visualizer.is_paused = True
+            visualizer.visualize_preorder_trav()
+            time.sleep(0.5)
+            visualizer.visualize_binary_tree()
+
+        if keys[pygame.K_3]:
+            visualizer.visualize_revinorder_trav()
+            time.sleep(0.5)
+            visualizer.visualize_binary_tree()
+
+        if keys[pygame.K_4]:
+            visualizer.visualize_postorder_trav()
+            time.sleep(0.5)
+            visualizer.visualize_binary_tree()
 
         if keys[pygame.K_q]:
             run = False
 
     pygame.quit()
 
-start()
+
+def start_term():
+    print('\nInorder Traverse')
+    tree.inorder_trav(tree.root)
+    print('\nPreorder Traverse')
+    tree.preorder_trav(tree.root)
+    print('\nReverse Inorder Traverse')
+    tree.rev_inorder_trav(tree.root)
+    print('\nPostorder Traverse')
+    tree.postorder_trav(tree.root)
+
+# start_term()
+start_visual()
